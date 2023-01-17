735254e300a5f7af0b53ec97d464f26e2bc18fd9
cmayadev <sefirot.maya@gmail.com>
2023-01-17 22:37:23 +0100

    basic layout structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Layout from './components/layout/Layout/Layout.jsx';
+
+import './App.css'
+
+function App() {
+
+
+
+  return (
+    <div id="App">
+      <Layout>
+        Body
+      </Layout>
+    </div>
+  )
+}
+
+export default App
